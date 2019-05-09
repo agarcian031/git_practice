d056ff7409636feafcf81ffb2af8e7ecfc37891c
agarcian031 <algarcian@outlook.com>
2019-05-09 11:29:42 -0600

    Add gem files and customizations

--- main.rb
+++ main.rb
@@ -1,25 +1,33 @@
+require "colorize"
+require "pry"
+require "rufo"
+
 def puts_git(cmd)
   puts `git #{cmd} -h`
-end 
+end
 
 def menu
- puts "1: Enter git command"
- puts "2: Exit"
- choice = gets.to_i 
- print `clear`
- sleep(3)
- case choice 
- when 1 
-  puts "Enter git command"
-  puts_git(gets.strip)
-  menu 
- when 2 
-  puts "Thanks for using our program"
-  exit 
- else 
- puts "Invalid input"
- menu 
- end 
-end 
+  puts "MAIN MENU".colorize(:cyan)
+  puts "1: Enter git command".colorize(:yellow)
+  puts "2: Exit".colorize(:green)
+  choice = gets.to_i
+  sleep(2)
+  print `clear`
+  sleep(3)
+  case choice
+  when 1
+    puts "Enter git command".colorize(:magenta)
+    puts_git(gets.strip)
+    menu
+  when 2
+    puts "Thanks for using our program".colorize(:blue)
+    exit
+  else
+    puts "Invalid input".colorize(:red)
+    sleep(2)
+    clear
+    menu
+  end
+end
 
-menu 
+menu
